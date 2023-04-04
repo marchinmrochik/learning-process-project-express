5c99dadd4875afdf04f0f609e9634ef93edb196d
marchinmrochik <kiselroman280@gmail.com>
2023-04-04 22:39:18 +0300

    added userController

---
+++ src/users/users.controller.ts
@@ -0,0 +1,25 @@
+import { BaseController } from "../common/base.controller";
+import { Response, Request, NextFunction } from 'express';
+import { LoggerService } from "../logger/logger.service";
+
+export class UserController extends BaseController {
+  constructor(logger: LoggerService) {
+    super(logger);
+    this.bindRoutes([
+      {
+        path: '/register', method: 'post', func: this.register
+      },
+      {
+        path: '/login', method: 'post', func: this.login
+      }
+    ])
+  }
+
+  login(req: Request, res: Response, next: NextFunction) {
+    this.ok(res, 'login');
+  }
+
+  register(req: Request, res: Response, next: NextFunction) {
+    this.ok(res, 'register');
+  }
+}
