358d1e1aee2765e7abf06f978912de4956376f97
marchinmrochik <kiselroman280@gmail.com>
2023-04-04 23:05:54 +0300

    added ExceptionFilter

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
 import { BaseController } from "../common/base.controller";
 import { Response, Request, NextFunction } from 'express';
 import { LoggerService } from "../logger/logger.service";
+import { HTTPError } from "../errors/http-error.class";
 
 export class UserController extends BaseController {
   constructor(logger: LoggerService) {
@@ -16,7 +17,7 @@ export class UserController extends BaseController {
   }
 
   login(req: Request, res: Response, next: NextFunction) {
-    this.ok(res, 'login');
+    next(new HTTPError(401, 'error auth', 'login'))
   }
 
   register(req: Request, res: Response, next: NextFunction) {
