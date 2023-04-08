4d8e8c0c2e830433c8e14c9d83c27689413b7136
marchinmrochik <kiselroman280@gmail.com>
2023-04-08 17:16:42 +0300

    added validate to dyo user login

--- src/users/dto/user-login.dto.ts
+++ src/users/dto/user-login.dto.ts
@@ -1,6 +1,9 @@
-
+import { IsEmail, IsString } from "class-validator";
 
 export class UserLoginDTO {
+  @IsEmail({}, { message: "Email wrong!" })
   email: string;
+  
+  @IsString({ message: "Password required!" })
   password: string;
 }
--- src/users/dto/user-register.dto.ts
+++ src/users/dto/user-register.dto.ts
@@ -1,6 +1,5 @@
 import { IsEmail, IsString } from "class-validator";
 
-
 export class UserRegisterDTO {
   @IsEmail({}, { message: "Email wrong!" })
   email: string;
