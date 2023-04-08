8882299a46ad5c276ca6d3dcd962833c44b40876
marchinmrochik <kiselroman280@gmail.com>
2023-04-08 10:20:22 +0300

    added eslint, prettier, nodemon, config to vs-code

--- src/main.ts
+++ src/main.ts
@@ -7,20 +7,25 @@ import { ILogger } from './logger/logger.interface';
 import { TYPES } from './types';
 import { IExceptionFilter } from './errors/exception.filter.interface';
 
+export interface IBootstrapReturn {
+	appContainer: Container;
+	app: App;
+}
+
 export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
-  bind<ILogger>(TYPES.ILogger).to(LoggerService);
-  bind<UserController>(TYPES.UserController).to(UserController);
-  bind<IExceptionFilter>(TYPES.ExceptionFilter).to(ExceptionFilter);
-  bind<App>(TYPES.Application).to(App);
+	bind<ILogger>(TYPES.ILogger).to(LoggerService);
+	bind<UserController>(TYPES.UserController).to(UserController);
+	bind<IExceptionFilter>(TYPES.ExceptionFilter).to(ExceptionFilter);
+	bind<App>(TYPES.Application).to(App);
 });
 
-function bootstrap() {
-  const appContainer = new Container();
-  appContainer.load(appBindings);
-  const app = appContainer.get<App>(TYPES.Application);
-  app.init();
+function bootstrap(): IBootstrapReturn {
+	const appContainer = new Container();
+	appContainer.load(appBindings);
+	const app = appContainer.get<App>(TYPES.Application);
+	app.init();
 
-  return { app, appContainer };
+	return { app, appContainer };
 }
 
 export const { app, appContainer } = bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,32 +1,35 @@
-import { BaseController } from "../common/base.controller";
+import { BaseController } from '../common/base.controller';
 import { Response, Request, NextFunction } from 'express';
-import { HTTPError } from "../errors/http-error.class";
-import { inject, injectable } from "inversify";
-import { ILogger } from "../logger/logger.interface";
-import { TYPES } from "../types";
-import { IUserController } from "./users.controller.interface";
+import { HTTPError } from '../errors/http-error.class';
+import { inject, injectable } from 'inversify';
+import { ILogger } from '../logger/logger.interface';
+import { TYPES } from '../types';
+import { IUserController } from './users.controller.interface';
 import 'reflect-metadata';
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
+	constructor(@inject(TYPES.ILogger) private loggerService: ILogger) {
+		super(loggerService);
+		this.bindRoutes([
+			{
+				path: '/register',
+				method: 'post',
+				func: this.register,
+			},
+			{
+				path: '/login',
+				method: 'post',
+				func: this.login,
+			},
+		]);
+	}
 
-  constructor(@inject(TYPES.ILogger) private loggerService: ILogger ) {
-    super(loggerService);
-    this.bindRoutes([
-      {
-        path: '/register', method: 'post', func: this.register
-      },
-      {
-        path: '/login', method: 'post', func: this.login
-      }
-    ])
-  }
+	login(req: Request, res: Response, next: NextFunction): void {
+		next(new HTTPError(401, 'error auth', 'login'));
+	}
 
-  login(req: Request, res: Response, next: NextFunction) {
-    next(new HTTPError(401, 'error auth', 'login'))
-  }
-
-  register(req: Request, res: Response, next: NextFunction) {
-    this.ok(res, 'register');
-  }
+	register(req: Request, res: Response, next: NextFunction): void {
+		this.ok(res, 'register');
+	}
 }
