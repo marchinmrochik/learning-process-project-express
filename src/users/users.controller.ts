ffd4944b2a7b70e662f4f5622ee06f25faf60cbf
marchinmrochik <kiselroman280@gmail.com>
2023-04-08 23:31:57 +0300

    added auth guard and show more data for infoUser

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,6 +12,7 @@ import { ValidateMiddleware } from '../common/validate.middleware';
 import { IUserService } from './users.service.interface';
 import { sign } from 'jsonwebtoken';
 import { IConfigService } from '../config/config.service.interface';
+import { AuthGuard } from '../common/auth.guard';
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
@@ -38,7 +39,7 @@ export class UserController extends BaseController implements IUserController {
 				path: '/info',
 				method: 'get',
 				func: this.info,
-				middlewares: [],
+				middlewares: [new AuthGuard()],
 			},
 		]);
 	}
@@ -87,13 +88,14 @@ export class UserController extends BaseController implements IUserController {
 				(err, token) => {
 					if (err) reject(err);
 
-					resolve(token as string);
+					resolve(token as string); 
 				},
 			);
 		});
 	}
 
 	async info({ user }: Request<{}, {}, UserRegisterDTO>,res: Response, next: NextFunction,): Promise<void> {
-		this.ok(res, { email: user });
+		const userInfo = await this.userService.getUserInfo(user)
+		this.ok(res, { email: userInfo?.email, name: userInfo?.name });
 	}
 }
--- src/users/users.service.interface.ts
+++ src/users/users.service.interface.ts
@@ -6,4 +6,5 @@ import { UserModel } from '@prisma/client';
 export interface IUserService {
 	createUser: (dto: UserRegisterDTO) => Promise<UserModel | null>;
 	validateUser: (dto: UserLoginDTO) => Promise<boolean>;
+	getUserInfo: (email: string) => Promise<UserModel | null>;
 }
